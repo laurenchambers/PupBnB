4927495acd7df5e21419d7521978210efe180c6e
Lauren Chambers <laurenchambers11@icloud.com>
2021-02-25 14:13:45 -0600

    got both of the spots pages working (all spots, and individual spots)

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,45 +5,61 @@ const asyncHandler = require("express-async-handler");
 const router = express.Router();
 const { Spot } = require("../../db/models");
 
-//spots route
+//route to display ALL spots
 router.get(
-  "/:id(\\d+)",
+  "/",
   asyncHandler(async (req, res) => {
-    const spot = await Spot.findByPk(req.params.id);
+    const spots = await Spot.findAll();
+    // console.log(spots);
+    res.json({ spots });
+  })
+);
+
+//route for ONE spot
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const spot = await Spot.findOne({
+      where: {
+        id,
+      },
+    });
+    // console.log(spots);
     res.json({ spot });
   })
 );
 
 //spots post route
-router.post(
-  "/",
-  asyncHandler(async (req, res) => {
-    const {
-      hostId,
-      name,
-      description,
-      streetAddress,
-      city,
-      state,
-      zipCode,
-      price,
-    } = req.body;
-    //maybe change to findall
-    const spot = await Spot.create({
-      hostId,
-      name,
-      description,
-      streetAddress,
-      city,
-      state,
-      zipCode,
-      price,
-    });
+// router.post(
+//   "/",
+//   asyncHandler(async (req, res) => {
+//     const {
+//       hostId,
+//       name,
+//       description,
+//       streetAddress,
+//       city,
+//       state,
+//       zipCode,
+//       price,
+//     } = req.body;
+//     //maybe change to findall
+//     const spot = await Spot.create({
+//       hostId,
+//       name,
+//       description,
+//       streetAddress,
+//       city,
+//       state,
+//       zipCode,
+//       price,
+//     });
 
-    return res.json({
-      spot,
-    });
-  })
-);
+//     return res.json({
+//       spot,
+//     });
+//   })
+// );
 
 module.exports = router;
