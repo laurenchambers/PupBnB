7af7ef45dc793700502e5caee9a642aa719c20c4
Lauren Chambers <laurenchambers11@icloud.com>
2021-02-28 10:20:31 -0600

    db changes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,6 +1,7 @@
 "use strict";
 const { Validator } = require("sequelize");
 const bcrypt = require("bcryptjs");
+const { urlencoded } = require("express");
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
@@ -53,48 +54,55 @@ module.exports = (sequelize, DataTypes) => {
       },
     }
   );
-  User.associate = function (models) {
-    // associations can be defined here
 
-    User.prototype.toSafeObject = function () {
-      // remember, this cannot be an arrow function
-      const { id, username, email, isHost } = this; // context will be the User instance
-      return { id, username, email, isHost };
-    };
+  User.prototype.toSafeObject = function () {
+    // remember, this cannot be an arrow function
+    const { id, username, email, isHost } = this; // context will be the User instance
+    return { id, username, email, isHost };
+  };
 
-    User.prototype.validatePassword = function (password) {
-      return bcrypt.compareSync(password, this.hashedPassword.toString());
-    };
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
 
-    User.getCurrentUserById = async function (id) {
-      return await User.scope("currentUser").findByPk(id);
-    };
+  User.getCurrentUserById = async function (id) {
+    return await User.scope("currentUser").findByPk(id);
+  };
 
-    User.login = async function ({ credential, password }) {
-      const { Op } = require("sequelize");
-      const user = await User.scope("loginUser").findOne({
-        where: {
-          [Op.or]: {
-            username: credential,
-            email: credential,
-          },
+  User.login = async function ({ credential, password }) {
+    const { Op } = require("sequelize");
+    const user = await User.scope("loginUser").findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
         },
-      });
-      if (user && user.validatePassword(password)) {
-        return await User.scope("currentUser").findByPk(user.id);
-      }
-    };
-
-    User.signup = async function ({ username, email, password, isHost }) {
-      const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({
-        username,
-        email,
-        hashedPassword,
-        isHost,
-      });
+      },
+    });
+    if (user && user.validatePassword(password)) {
       return await User.scope("currentUser").findByPk(user.id);
+    }
+  };
+
+  User.signup = async function ({ username, email, password, isHost }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+      isHost,
+    });
+    return await User.scope("currentUser").findByPk(user.id);
+  };
+
+  User.associate = function (models) {
+    User.hasMany(models.Booking, { foreignKey: "userId" });
+    const columnMapping = {
+      through: "Booking",
+      otherKey: "spotId",
+      foreignKey: "UserId",
     };
+    User.belongsToMany(models.Spot, { columnMapping });
   };
   return User;
 };
