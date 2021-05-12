5d2621e664818fa622177451f952f46b0c970c51
Lauren Chambers <laurenchambers11@icloud.com>
2021-05-12 10:00:08 -0500

    updated package

--- backend/config/database.js
+++ backend/config/database.js
@@ -14,10 +14,17 @@ module.exports = {
     database,
     host,
     dialect: "postgres",
+    seederStorage: "sequelize",
   },
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     seederStorage: "sequelize",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
